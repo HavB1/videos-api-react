9f394c63aa093edb37a2690b307f3275ad08d1e2
Dedito <deditofb12@gmail.com>
2022-01-26 13:42:48 +0800

    final version

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { renderIntoDocument } from "react-dom/cjs/react-dom-test-utils.development";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos})=>{
-   const renderedList = videos.map((video)=>{
-      return <VideoItem video ={video}/>
-   })
-   return <div className="ui relaxed divided list">{renderedList}</div>
-}
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 
 export default VideoList;
